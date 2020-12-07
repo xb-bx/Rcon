916e19b74aa84083318667d317e39e7b9cbb0a3f
JJ <nazarnik050406@gmail.com>
2020-12-07 17:10:11 +0200

    initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rcon.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
